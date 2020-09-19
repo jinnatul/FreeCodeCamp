53af2b3dee40c0ecca3396c7b1525356153ad647
jinnatul <morolswediu@gmail.com>
2020-09-19 11:28:16 +0600

    Regular Expressions: Using the Test Method

---
+++ JS-Algo-DS-Certification/Using-the-Test-Method.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/using-the-test-method
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
