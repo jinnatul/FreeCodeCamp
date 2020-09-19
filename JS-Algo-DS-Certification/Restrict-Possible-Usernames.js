31a88180b60399b0b770cc0a2cda39c8a7051a90
jinnatul <morolswediu@gmail.com>
2020-09-19 16:52:09 +0600

    Regular Expressions: Restrict Possible Usernames

---
+++ JS-Algo-DS-Certification/Restrict-Possible-Usernames.js
@@ -0,0 +1,32 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/restrict-possible-usernames
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result)
+
+/* Code Explanation
+^ - start of input
+[a-z] - first character is a letter
+[a-z]+ - following characters are letters
+\d*$ - input ends with 0 or more digits
+| - or
+^[a-z] - first character is a letter
+\d\d+ - following characters are 2 or more digits
+$ - end of input */
+
+
+let username = "JackOfAllTrades";
+const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let result = userCheck.test(username);
+console.log(result)
+
+/* Code Explanation
+^ - start of input
+[a-z] - first character is a letter
+[0-9]{2,0} - ends with two or more numbers
+| - or
+[a-z]+ - has one or more letters next
+\d* - and ends with zero or more numbers
+$ - end of input
+i - ignore case of input */
