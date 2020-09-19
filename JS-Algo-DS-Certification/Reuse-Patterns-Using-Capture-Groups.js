1ec41fd9f35d477026fc54215c4023178012091b
jinnatul <morolswediu@gmail.com>
2020-09-19 17:08:21 +0600

    RE: Reuse Patterns Using Capture Groups

---
+++ JS-Algo-DS-Certification/Reuse-Patterns-Using-Capture-Groups.js
@@ -0,0 +1,5 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/reuse-patterns-using-capture-groups
+
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+let result = reRegex.test(repeatNum);
