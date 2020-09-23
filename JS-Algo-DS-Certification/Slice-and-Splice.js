163347331c3014db03355085e9c0981bf3ab02e0
jinnatul <morolswediu@gmail.com>
2020-09-23 17:10:09 +0600

    ALGO: Slice and Splice

---
+++ JS-Algo-DS-Certification/Slice-and-Splice.js
@@ -0,0 +1,9 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/slice-and-splice
+
+function frankenSplice(arr1, arr2, n) {
+  let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
