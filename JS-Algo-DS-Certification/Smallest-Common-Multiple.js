2f8258f2636bdfaca68c2212d51c2b821445ad2b
jinnatul <morolswediu@gmail.com>
2020-09-24 21:43:55 +0600

    ALGO: Smallest-Common-Multiple

---
+++ JS-Algo-DS-Certification/Smallest-Common-Multiple.js
@@ -0,0 +1,21 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/smallest-common-multiple
+
+function smallestCommons(arr) {
+  const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+  // Least Common Multiple for two numbers based on GCD
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+
+  // range
+  let [min, max] = arr.sort((a, b) => a - b);
+  let currentLCM = min;
+
+  while (min < max) {
+    currentLCM = lcm(currentLCM, ++min);
+  }
+
+  return currentLCM;
+}
+
+
+smallestCommons([1,5]);
