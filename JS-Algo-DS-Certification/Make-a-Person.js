43187a4a4debbf92c0874d143839b095a8e3428e
jinnatul <morolswediu@gmail.com>
2020-09-24 22:20:47 +0600

    ALGO: Make-a-Person

---
+++ JS-Algo-DS-Certification/Make-a-Person.js
@@ -0,0 +1,32 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/make-a-person
+
+let Person = function(firstAndLast) {
+  let fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+
+let bob = new Person("Bob Ross");
+bob.getFullName();
